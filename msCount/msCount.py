add8fc140c4555d792c9ad9b34c784246b3d8eec
cjwei <cjwei@ucsd.edu>
2018-04-03 23:02:32 -0700

    developing msCounts to input bam files

--- msCount/msCount.py
+++ msCount/msCount.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-from __future__ import print_function
 import argparse
 from importer import parseProbes
+import pysam
 
 '''
 Usage: python script.py --probes probes.info.txt --input input.sorted.bam
@@ -15,10 +15,19 @@ The script will then go through and output a list of microsatellite subunit coun
 def msCount():
     parser = argparse.ArgumentParser(description="Perform msCounts from mapped reads")
     parser.add_argument('--probes', action="store", dest="probe_file")
-    parser.add_argument('--input', action="store", dest="input_bam")
+    parser.add_argument('--bam', action="store", dest="input_bam")
+    parser.add_argument('--num_reads', action="store", dest="num_reads", default=10)
     args = parser.parse_args()
     
+    #Import targetDict from probe_file
     targetDict = parseProbes(args.probe_file)
+    
+    #Perform msCount for each targetID within targetDict
+    samfile = pysam.AlignmentFile(args.input_bam, "rb")
+    for targetID in sorted(targetDict.keys()):
+        for read in samfile.fetch(targetDict[targetID]["chrom"], int(targetDict[targetID]["chromStart"]), int(targetDict[targetID]["chromEnd"])):
+            print(read)
+    samfile.close()
     
 #%%
 if __name__ == "__main__":
