1d93375f74ab224828cf458661005dfb621f6b3c
Chris Wei <cjwei@eng.ucsd.edu>
2019-04-30 08:52:21 -0700

    Adjust methyl call to import from methylpy tsv file (0_meth_importTSV) in order to decrease memory requirements

--- Methyl/0_meth_importTSV.py
+++ Methyl/0_meth_importTSV.py
@@ -5,40 +5,98 @@ import subprocess
 import pickle
 from Bio import SeqIO #Remove when running on TSCC (does not have Bio module)
 from Bio.Seq import Seq #Remove when running on TSCC (does not have Bio module)
+import pybedtools
 
 '''
-Usage: python script.py --cell_files ... --ref_fa ... --ref_CGI ... --methDict ...
-This script will import methlylation information for all given cell types or single cells and export methDict.
-The structure of methDict is as follows:
-    methDict
-        file_name
-            "base"
-                base_loc
-                    "Total" = total number reads
-                    "Meth" = number of methylated reads
-                    "Type" = C type (only save CpG, ignore CHH and CHG)
+Usage: python script.py --sample_info ... --methDict ...
+This script will import all CpG methylation signal from sample tsv files into a methDict dictionary to speed up processing of pairwise dissimilarity calculation.
+The structure of the outputted methDict is as follows:
+methDict
+    "sample"
+        sample_name
+            file_loc
+    "base"
+        base_loc (chr:pos;chain)
+            sample_name
+                (num_meth, total_reads)
 '''
+
+def CGIstats(methDict, ref_CGI):
+    for sample_name in sorted(methDict["sample"].keys()):
+        methDict["sample"][sample_name]["CGI"] = {}
+        sample_bed = pybedtools.BedTool(methDict["sample"][sample_name]["bam_file"])
+        ref_bed = pybedtools.BedTool(ref_CGI)
+        CGI_intersect = ref_bed.intersect(sample_bed, bed=True, wa=True, wb=True)
+        for CGI in CGI_intersect:
+            CGI_loc = CGI.fields[0] + ":" + CGI.fields[1] + "-" + CGI.fields[2]
+            read_name = CGI.fields[7]
+            if CGI_loc not in methDict["sample"][sample_name]["CGI"].keys():
+                methDict["sample"][sample_name]["CGI"][CGI_loc] = 1
+            else:
+                methDict["sample"][sample_name]["CGI"][CGI_loc] += 1
+    return methDict
+
+def calcStats(methDict, prefix):
+    '''
+    This funciton will calculate the following statistics per file:
+        1) Total number of CpG positions covered
+        2) Avg number of reads covering each position
+        3) Number CGI's covered
+    '''
+    stats_output = open(prefix + ".covStats.txt", 'w')
+    stats_output.write("Sample\t"
+        + "Unique CpG (1x)\tMean Coverage (1x)\t"
+        + "Unique CpG (5x)\tMean Coverage (5x)\t"
+        + "Unique CpG (10x)\tMean Coverage (10x)\t"
+        + "Number CGI\tMean Coverage\n")
+    #Parse saved methDict data to calculate read coverage stats
+    for base_loc in sorted(methDict["base"].keys()):
+        for sample_name in sorted(methDict["base"][base_loc].keys()):
+            if not "Cov" in methDict["sample"][sample_name].keys():
+                methDict["sample"][sample_name]["Cov"] = [] #This contains a list of all CpG coverage values for each CpG site
+            methDict["sample"][sample_name]["Cov"].append(methDict["base"][base_loc][sample_name][1])
+    for sample_name in sorted(methDict["sample"].keys()):
+        stats_output.write(sample_name + "\t")
+        #Summarize CGI stats
+        num_CGI = len(methDict["sample"][sample_name]["CGI"].keys())
+        if (num_CGI > 0):
+            mean_CGI_cov = round(sum(methDict["sample"][sample_name]["CGI"].values())/num_CGI, 2)
+        else:
+            mean_CGI_cov = "NA"
+        #Determine CpG coverage for various cutoff values
+        for cutoff in [1,5,10]:
+            filtered_CpG = [CpG for CpG in methDict["sample"][sample_name]["Cov"] if CpG >= cutoff]
+            num_CpG = len(filtered_CpG)
+            if num_CpG > 0:
+                mean_CpG_cov = round(sum(filtered_CpG)/len(filtered_CpG), 2)
+            else:
+                mean_CpG_cov = "NA"
+            stats_output.write(str(num_CpG) + "\t" + str(mean_CpG_cov) + "\t")
+        stats_output.write(str(num_CGI) + "\t" + str(mean_CGI_cov) + "\n")
+    return
 
 def parseMethCall(methDict):
     for sample_name in sorted(methDict["sample"].keys()):
         print("Importing:\t" + sample_name)
-        f_methCall = open(methDict["sample"][sample_name]["file_loc"])
+        f_methCall = open(methDict["sample"][sample_name]["tsv_file"])
         for line in f_methCall:
             if line.startswith('#'):
                 continue
             else:
-                (chr, pos, chain, ctype, meth, total_bases) = line.split()[0:6]
+                (chr, pos, chain, ctype, meth, total_reads) = line.split()[0:6]
                 if ctype.startswith('CG'): #We only care about CGN methylated cytosine calls
                     base_loc = chr.replace("chr","") + ":" + pos + ";" + chain
                     if base_loc not in methDict["base"].keys():
                         methDict["base"][base_loc] = {}
-                    methDict["base"][base_loc][sample_name] = (int(meth), int(total_bases))
+                    methDict["base"][base_loc][sample_name] = (int(meth), int(total_reads))
     return methDict
 
 def main():
     parser = argparse.ArgumentParser(description="Import methylation information for cell types or single cells and export methDict")
     parser.add_argument('--sample_info', action="store", dest="sample_info", help="Tab-delimited file containing the sample/cell type tsv files/locations along with sample/cell type names")
-    parser.add_argument('--methDict', action="store", dest="methDict_file", help="File name containing exported methDict (*.pkl)")
+    parser.add_argument('--prefix', action="store", dest="prefix", help="Prefix name for exporting methDict and coverage statistics information")
+    parser.add_argument('-stats', action="store_true", help="[Optional] Output statistics including unique CpG and CGI counts")
+    parser.add_argument('--ref_CGI', action="store", dest="ref_CGI", help="Bed file containing reference genome CGI locations")
     args = parser.parse_args()
 
     #Import methylation calls into methDict
@@ -47,13 +105,23 @@ def main():
     methDict["base"] = {}
     with open(args.sample_info) as f_info:
         for sample in f_info:
-            (file_loc, sample_name) = sample.split()
+            (bam_file, tsv_file, sample_name) = sample.split()
             methDict["sample"][sample_name] = {}
-            methDict["sample"][sample_name]["file_loc"] = file_loc
+            methDict["sample"][sample_name]["bam_file"] = bam_file
+            methDict["sample"][sample_name]["tsv_file"] = tsv_file
     methDict = parseMethCall(methDict)
 
-    with open(args.methDict_file, 'wb') as methDict_file:
+    #Export methDict prior to calculating stats file
+    with open(args.prefix + ".methDict.pkl", 'wb') as methDict_file:
         pickle.dump(methDict, methDict_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    #We also want to calculate methylation coverage stats
+    if args.stats is True:
+        if args.ref_CGI:
+            methDict = CGIstats(methDict, args.ref_CGI)
+        else:
+            parser.error('Cannot calculate statistics without specifying ref_CGI file')
+        calcStats(methDict, args.prefix)
 
 #%%
 if __name__ == "__main__":
--- Methyl/1_meth_calcPD.py
+++ Methyl/1_meth_calcPD.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import pickle
+from tqdm import tqdm
 
 '''
 Usage: python script.py --sample ... --cellType ... --prefix ... -stats --ref_CGI ...
@@ -8,13 +9,13 @@ This script is based off of SingleC_MetLevel.pl from Guo 2015 Nat Prot paper <ht
 To run this script, we must include the following input:
     1) sample_methDict = pre-computed methDict containing single cell sample information.  The structure of methDict is as follows:
     methDict
-            file_name
-                "base"
-                    base_loc
-                        "Total" = total number reads
-                        "Meth" = number of methylated reads
-                        "Type" = C type (only save CpG [indicated as "CGN"], ignore CHH and CHG)
-    2) cellType_methDict = pre-computed methDict containing reference cell type data
+        "sample"
+            sample_name
+                "file_loc" = file_loc
+        "base"
+            base_loc (chr:pos;chain)
+                sample_name = (num_meth, total_reads)
+    2) cellType_info = tab-delimited file containing file locations and cell type information for reference tsv files
     3) ref_CGI = reference bed file containing CGI locations across the desired reference genome
     4) prefix = prefix for output files
 The script will then go through and output the following statistics/files:
@@ -23,129 +24,162 @@ The script will then go through and output the following statistics/files:
     3) prefix.PD.pkl = exported dictionary containing pairwise-dissimilarity between samples and cell types
 '''
 
-def CGIstats(methDict, ref_CGI):
-    for file_name in sorted(methDict.keys()):
-        methDict[file_name]["CGI"] = {}
-        sample_bed = pybedtools.BedTool(file_name+".bam")
-        ref_bed = pybedtools.BedTool(ref_CGI)
-        CGI_intersect = ref_bed.intersect(sample_bed, bed=True, wa=True, wb=True)
-        for CGI in CGI_intersect:
-            CGI_loc = CGI.fields[0] + ":" + CGI.fields[1] + "-" + CGI.fields[2]
-            read_name = CGI.fields[7]
-            if CGI_loc not in methDict[file_name]["CGI"].keys():
-                methDict[file_name]["CGI"][CGI_loc] = [read_name]
+def calcPD(sampleDict, typeDict, DMR_bool, seqDepth, min_shared, min_rate, prefix):
+    #We want to iterate through all cell types and calculate pairwise dissimilarity when compared to all samples
+    PDdict_temp = {} #Keep all pairwise dissimilarity comparisons in temp dictionary
+    covDict = {} #We also want to keep track of all CpG base_loc and DMR's covered per sample
+    for cellType_name in sorted(typeDict["cellType"].keys()):
+        print("Intersecting:\t" + cellType_name)
+        f_cellType = open(typeDict["cellType"][cellType_name]["file_loc"])
+        for i, line in enumerate(tqdm(f_cellType)):
+            if line.startswith('#'):
+                continue
+            base_info = line.split()
+            (chr, pos, chain, ctype, meth, total_reads) = base_info[0:6]
+            if ctype.startswith('CG'): #We only care about CGN methylated cytosine calls
+                base_loc = chr.replace("chr","") + ":" + pos + ";" + chain
+                cellType_methRate = float(int(meth)/int(total_reads))
             else:
-                methDict[file_name]["CGI"][CGI_loc].append(read_name)
-    return methDict
-
-def calcCovStats(methDict, file_name, cutoff):
-    (num_CpG, total_cov) = (0, 0)
-    for base_loc in sorted(methDict[file_name]["base"].keys()):
-        if (methDict[file_name]["base"][base_loc]["Total"] >= cutoff) and methDict[file_name]["base"][base_loc]["Type"] == "CpG":
-            num_CpG += 1
-            total_cov += methDict[file_name]["base"][base_loc]["Total"]
-    if(num_CpG == 0):
-        return (num_CpG, "NA")
-    mean_cov = round(total_cov / num_CpG)
-    return (num_CpG, mean_cov)
-
-def calcStats(methDict, prefix):
-    '''
-    This funciton will calculate the following statistics per file:
-        1) Total number of CpG positions covered
-        2) Avg number of reads covering each position
-        3) Number CGI's covered
-    '''
-    import pybedtools
-    stats_output = open(prefix + ".covStats.txt", 'a+')
-    stats_output.write("File\t"
-        + "Unique CpG (1x)\tMean Coverage (1x)\t"
-        + "Unique CpG (5x)\tMean Coverage (5x)\t"
-        + "Unique CpG (10x)\tMean Coverage (10x)\t"
-        + "Number CGI\tMean Coverage\n")
-    for file_name in sorted(methDict.keys()):
-        stats_output.write(file_name + "\t")
-        #Summarize CGI stats
-        num_CGI = len(methDict[file_name]["CGI"].keys())
-        if (num_CGI > 0):
-            mean_CGI_cov = round(sum(methDict[file_name]["CGI"].values())/num_CGI)
-        else:
-            mean_CGI_cov = "NA"
-        #Determine CpG coverage
-        for cutoff in [1,5,10]:
-            (num_CpG, mean_cov) = calcCovStats(methDict, file_name, cutoff)
-            stats_output.write(str(num_CpG) + "\t" + str(mean_cov) + "\t")
-        stats_output.write(str(num_CGI) + "\t" + str(mean_CGI_cov) + "\n")
-    stats_output.close()
-    return
-
-def calcPD(sampleDict, typeDict, seqDepth, min_shared, prefix):
-    PDdict = {} #We want to save all paiwise_dis and p-values as dictionary where we have ordered lists for each file analyzed (ordered alphabetically by filename)
+                continue
+            if 'DMR' in line:
+                DMR_loc = base_info[7].replace("DMR:", "")
+            else:
+                DMR_loc = None
+                if DMR_bool is True:
+                    continue #Skip base_loc if not contained within DMR and DMR_bool is set to True
+            if not min(min_rate, 1 - min_rate) < cellType_methRate < max(min_rate, 1 - min_rate): #Only consider base locations in which reference methRate is not between (min_rate, 1-min_rate)
+                if base_loc in sampleDict["base"].keys(): #We next want to search matching base_loc in sampleDict
+                    for sample_name in sampleDict["base"][base_loc]:
+                        #We want to save coverage data into covDict
+                        if sample_name not in covDict.keys():
+                            covDict[sample_name] = {}
+                            covDict[sample_name]["all_CpG"] = set()
+                            covDict[sample_name]["CpG"] = {}
+                            covDict[sample_name]["DMR"] = {}
+                        if cellType_name not in covDict[sample_name]["CpG"].keys():
+                            covDict[sample_name]["CpG"][cellType_name] = set()
+                        covDict[sample_name]["all_CpG"].add(base_loc) #Save all CpG's across all cellTypes captued per sample
+                        covDict[sample_name]["CpG"][cellType_name].add(base_loc) #Save CpG's for each cellType individually
+                        if DMR_loc is not None:
+                            if cellType_name not in covDict[sample_name]["DMR"].keys():
+                                covDict[sample_name]["DMR"][cellType_name] = set()
+                            covDict[sample_name]["DMR"][cellType_name].add(DMR_loc) #Save DMR locations for each cellType individually
+                        #Next we want to calculate pairwise dissimilarity between sample and cellType and save into PDdict_temp
+                        sample_methRate = float(sampleDict["base"][base_loc][sample_name][0] / sampleDict["base"][base_loc][sample_name][1])
+                        dis = abs(sample_methRate - cellType_methRate) * 100
+                        if sample_name not in PDdict_temp.keys():
+                            PDdict_temp[sample_name] = {}
+                        if cellType_name not in PDdict_temp[sample_name].keys():
+                            PDdict_temp[sample_name][cellType_name] = []
+                        PDdict_temp[sample_name][cellType_name].append(dis)
+        f_cellType.close()
+    #Save pairwise dissimilarity into PDdict for future processing/plotting
+    cellType_list = []
+    PDdict = {}
     PDdict["PD"] = {}
-    type_list = []
     PD_output = open(prefix + ".PD.txt", 'w')
     PD_output.write("Sample\tCell Type\tPairwise Dissimilarity\tNum Shared\n")
-    for sample_name in sorted(sampleDict.keys()):
-        PDdict["PD"][sample_name] = []
-        for type_name in sorted(typeDict.keys()):
-            if len(typeDict[type_name]["base"].keys()) < min_shared:
-                continue
-            dis_sum = 0
-            num_shared = 0
-            for shared_base in sampleDict[sample_name]["base"].keys() & typeDict[type_name]["base"].keys():
-                if sampleDict[sample_name]["base"][shared_base]["Type"] == "CGN" and typeDict[type_name]["base"][shared_base]["Type"] == "CGN":
-                    if sampleDict[sample_name]["base"][shared_base]["Total"] >= seqDepth and typeDict[type_name]["base"][shared_base]["Total"] >= seqDepth:
-                        sample_methRate = float(sampleDict[sample_name]["base"][shared_base]["Meth"]/sampleDict[sample_name]["base"][shared_base]["Total"])
-                        type_methRate = float(typeDict[type_name]["base"][shared_base]["Meth"]/typeDict[type_name]["base"][shared_base]["Total"])
-                        if sample_methRate.is_integer() and type_methRate.is_integer():
-                            num_shared += 1
-                            if sample_methRate == type_methRate:
-                                dis_sum += 0
-                            else:
-                                dis_sum += 100
-            pairwise_dis = float(dis_sum/num_shared)
-            PD_output.write(sample_name + "\t" + type_name + "\t" + str(round(pairwise_dis,4)) + "\t" + str(num_shared) + "\n")
-            PDdict["PD"][sample_name].append(pairwise_dis)
-            type_list.append(type_name)
+    for sample_name in sorted(PDdict_temp.keys()):
+        PD_list = []
+        for cellType_name in sorted(PDdict_temp[sample_name].keys()):
+            if len(PDdict_temp[sample_name][cellType_name]) >= min_shared:
+                pairwise_dist = float(sum(PDdict_temp[sample_name][cellType_name])/len(PDdict_temp[sample_name][cellType_name]))
+                PD_output.write(sample_name + "\t" + cellType_name + "\t" + str(round(pairwise_dist, 4)) + "\t" + str(len(PDdict_temp[sample_name][cellType_name])) + "\n")
+                PD_list.append(pairwise_dist)
+                cellType_list.append(cellType_name)
+            else:
+                PD_list.append(0)
+        if len(PD_list) > 0:
+            PDdict["PD"][sample_name] = PD_list
     PD_output.close()
 
     #Export PDdict to file using pickle
-    PDdict["index"] = sorted(list(set(type_list))) #This contains cell type names used for pandas dataframe index
+    PDdict["index"] = sorted(list(set(cellType_list))) #This contains cell type names used for pandas dataframe index
 
     with open(prefix + ".PD.pkl", 'wb') as PDdict_file:
         pickle.dump(PDdict, PDdict_file, protocol=pickle.HIGHEST_PROTOCOL)
 
-    return
+    return covDict
+
+def printStats(covDict, DMR_bool, prefix):
+    f_stats = open(prefix + ".PD.stats.txt", 'w')
+    #Gather information for all sample_name and cellType_name
+    sample_list = covDict.keys()
+    cellType_list = []
+    for sample_name in sample_list:
+        cellType_list.extend(list(covDict[sample_name]["DMR"].keys()))
+    cellType_list = list(set(cellType_list))
+    #Make sure that there are no empty values in the covDict (fill in with an empty set if there is)
+    for sample_name in sorted(sample_list):
+        for cellType_name in sorted(cellType_list):
+            if cellType_name not in covDict[sample_name]["CpG"].keys():
+                covDict[sample_name]["CpG"][cellType_name] = set()
+            if cellType_name not in covDict[sample_name]["DMR"].keys():
+                covDict[sample_name]["DMR"][cellType_name] = set()
+    #1) Print basic statitistics for number of CpG/DMR covered for each sample
+    f_stats.write("---------- Raw Statistics (Total Num CpG, Num CpG (DMR) per cell type) ----------\n")
+    if DMR_bool is True: #NOTE: If DMR is set, these are only sum of CpG counts found within DMR of all cell types
+        f_stats.write("NOTE: Because DMR option is True, we are only considering CpG within DMRs across all cell types\n")
+    f_stats.write("Sample,Num Total CpG," + ','.join(i + " CpG (DMR)" for i in sorted(cellType_list)) + "\n")
+    for sample_name in sorted(sample_list):
+        f_stats.write(sample_name + ',' + str(len(covDict[sample_name]["all_CpG"])) + ',') #We want to print out the total number of CpG across all cell types for sample
+        for cellType_name in sorted(cellType_list):
+            f_stats.write(str(len(covDict[sample_name]["CpG"][cellType_name])))
+            f_stats.write('(' + str(len(covDict[sample_name]["DMR"][cellType_name])) + '),') #Print number of CpG and DMR covered per cell type per sample
+        f_stats.write("\n")
+    #2) Print number of shared CpG for each pairwise sample comparison
+    f_stats.write("\n---------- Num Shared CpG between each sample----------\n")
+    f_stats.write(',' + ','.join(sorted(sample_list)) + "\n")
+    for sample_name1 in sorted(sample_list):
+        f_stats.write(sample_name1 + ',')
+        for sample_name2 in sorted(sample_list):
+            f_stats.write(str(len(covDict[sample_name1]["all_CpG"].intersection(covDict[sample_name2]["all_CpG"]))) + ',')
+        f_stats.write("\n")
+    #3) Print number of DMR shared
+    for cellType_name in sorted(cellType_list):
+        f_stats.write("\n---------- Num DMR shared between each sample (cellType:" + cellType_name + ") ----------\n")
+        f_stats.write(',' + ','.join(sorted(sample_list)) + "\n")
+        for sample_name1 in sorted(sample_list):
+            f_stats.write(sample_name1 + ',')
+            for sample_name2 in sorted(sample_list):
+                f_stats.write(str(len(covDict[sample_name1]["DMR"][cellType_name].intersection(covDict[sample_name2]["DMR"][cellType_name]))) + ',')
+            f_stats.write("\n")
+    f_stats.close()
 
 def main():
     parser = argparse.ArgumentParser(description="Calculate methylation coverage")
     parser.add_argument('--sample', action="store", dest="sample_pkl", help="Pre-computed methDict containing sample information")
-    parser.add_argument('--cellType', action="store", dest="cellType_pkl", help="Pre-computed methDict containing cell type information")
+    parser.add_argument('--cellType_info', action="store", dest="cellType_info", help="Tab-delimited file containing the cell type tsv files/locations along with cell type names")
     parser.add_argument('--prefix', action="store", dest="prefix", help="Specifies prefix for output files")
-    parser.add_argument('--seqDepth', action="store", dest="seqDepth", default=1, help="[Optional] Minimum number reads covering CpG for calculating pd")
-    parser.add_argument('--numShared', action="store", dest="min_shared", default=10000, help="[Optional] Minimum number CpG shared between each sample and cell type comparison")
-    parser.add_argument('-stats', action="store_true", help="[Optional] Output statistics including unique CpG/CGI counts")
-    parser.add_argument('--ref_CGI', action="store", dest="ref_CGI", help="Bed file containing reference genome CGI locations")
+    parser.add_argument('-DMR', action="store_true", default=False, help="[Optional] Constrain bases of interest in reference file to DMR regions")
+    parser.add_argument('-covStats', action="store_true", default=True, help="Calculate CpG and DMR coverage statistics for each sample (pairwise between samples and cell types)")
+    parser.add_argument('--seqDepth', action="store", dest="seqDepth", default=1, help="[Optional] Minimum number reads covering CpG for calculating pd (Default: 1)")
+    parser.add_argument('--numShared', action="store", dest="min_shared", default=100, help="[Optional] Minimum number CpG shared between each sample and cell type comparison (Default: 100)")
+    parser.add_argument('--min_rate', action="store", dest="min_rate", default=0.5, help="[Optional] Minimum methylated or unmethylated rate for cellType reference for filtering confident methyl calls (Default: 0.5 = no filtering; 1 = only perfect call)")
     args = parser.parse_args()
 
-    #Import sample_methDict and cellType_methDict
+    #Import cell type file information into cellDict
     print("Import typeDict")
-    with open(args.cellType_pkl, 'rb') as cellType_file:
-        typeDict = pickle.load(cellType_file)
+    typeDict = {}
+    typeDict["cellType"] = {}
+    with open(args.cellType_info) as f_info:
+        for cellType in f_info:
+            (file_loc, cellType_name) = cellType.split()
+            typeDict["cellType"][cellType_name] = {}
+            typeDict["cellType"][cellType_name]["file_loc"] = file_loc
+
+    #Import pre-computed sample methDict
     print("Import sampleDict")
     with open(args.sample_pkl, 'rb') as sample_file:
         sampleDict = pickle.load(sample_file)
 
-    #Calculate methylation coverage statistics
-    if args.stats is True:
-        if args.ref_CGI:
-            sampleDict = CGIstats(sampleDict, args.ref_CGI)
-        else:
-            parser.error('Cannot calculate statistics without specifying ref_CGI file')
-        calcStats(sampleDict, args.prefix)
-
     #Calculate pairwise dissimilarity matrix
-    calcPD(sampleDict, typeDict, int(args.seqDepth), int(args.min_shared), args.prefix)
+    print("Calculating PD")
+    covDict = calcPD(sampleDict, typeDict, args.DMR, int(args.seqDepth), int(args.min_shared), float(args.min_rate), args.prefix)
+
+    #Print statistics if specified
+    if args.covStats is True:
+        printStats(covDict, args.DMR, args.prefix)
 
 #%%
 if __name__ == "__main__":
